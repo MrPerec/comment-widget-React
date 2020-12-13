e3cf88bccd308744ff278d5e467de9f4c7466123
Ilya Makshanov <49143911+MrPerec@users.noreply.github.com>
2020-12-13 04:14:03 +0400

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,5 +4,8 @@ const NUMBER_COMMENTS = 1;
 const BOX_WIDTH = 48;
 const FIELD_WIDTH = 50;
 const FIELD_HEIGHT = 5;
+const TIME_LOCALE = `ru`;
+const TIME_OPTIONS = {day: `numeric`, month: `numeric`, year: `numeric`};
+const STORAGE_OLD_COMMENTS = `oldComments`;
 
-export {NUMBER_COMMENTS, BOX_WIDTH, FIELD_WIDTH, FIELD_HEIGHT}
+export {NUMBER_COMMENTS, BOX_WIDTH, FIELD_WIDTH, FIELD_HEIGHT, TIME_LOCALE, TIME_OPTIONS, STORAGE_OLD_COMMENTS}
