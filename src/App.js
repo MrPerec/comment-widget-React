a280bd8e52c5649f3ee3ff69138730581b2d9c3f
Ilya Makshanov <49143911+MrPerec@users.noreply.github.com>
2020-12-17 04:25:32 +0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CommentInput from './components/CommentInput.js';
-import CommentsList from './containers/CommentsList.js';
+import CommentsList from './components/CommentsList.js';
 import {TIME_LOCALE, TIME_OPTIONS, STORAGE_OLD_COMMENTS} from './constants/constants.js';
 
 export default class App extends React.Component {
@@ -26,11 +26,10 @@ export default class App extends React.Component {
 	addComment(event){
 		event.preventDefault();
 
-		const {comments, newAuthor, newText} = this.state;
+		const {comments, newAuthor, newText} = {...this.state};
 		const nowDateTime = new Date().toLocaleTimeString(TIME_LOCALE, TIME_OPTIONS);
-		const getUuid = () => ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,c=>(c^crypto.getRandomValues(new Uint8Array(1))[0]&15 >> c/4).toString(16));
 		const uuid = getUuid();
-
+		
 		comments.unshift({
 			author: newAuthor,
 			text: newText,
@@ -58,6 +57,7 @@ export default class App extends React.Component {
 	componentDidMount() {
 		const oldComments = JSON.parse( localStorage.getItem(STORAGE_OLD_COMMENTS) );
 
+		//this.setState({comments: oldComments});
 		if (oldComments !== null) this.setState({comments: oldComments})
 	}
 
@@ -79,3 +79,7 @@ export default class App extends React.Component {
 		)
 	}
 }
+
+function getUuid() {
+ 	return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,c=>(c^crypto.getRandomValues(new Uint8Array(1))[0]&15 >> c/4).toString(16));
+ } 
