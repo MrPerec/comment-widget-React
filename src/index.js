7ac0932754c29737e8372334af1dc1da996c332c
Ilya Makshanov <mrperec@mail.ru>
2023-10-10 22:52:34 +0400

    refactor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-/**
- * Главный файл приложения, отвечающий за рендеринг компонента App в корневой элемент HTML.
- * @module index
- */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/style.css';
 import App from './App.jsx';
 
-/**
- * Функция, которая рендерит компонент App в корневой элемент HTML с классом "root".
- * @function
- * @memberof module:index
- * @returns {void}
- */
 ReactDOM.render(<App />, document.querySelector(`.root`));
